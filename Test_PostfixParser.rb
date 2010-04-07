e614594bf10acc9519930ea0dbdb51fe2c538895
André Teintang <andte949@student.liu.se>
2010-04-07 13:27:08 +0200

    Rättat till testfallen, argumenten står nu på rätt plats och @engine använder reset

--- Test_PostfixParser.rb
+++ Test_PostfixParser.rb
@@ -5,127 +5,127 @@ require 'PostfixParser.rb'
 class Test_PostfixParser < Test::Unit::TestCase
   def test_exit
     program = PostfixParseString("exit")
-    assert_equal(program, [:exit])
+    assert_equal([:exit], program)
   end
 
   def test_plus
     program = PostfixParseString("+")
-    assert_equal(program, [:plus])
+    assert_equal([:plus], program)
   end
 
   def test_minus
     program = PostfixParseString("-")
-    assert_equal(program, [:minus])
+    assert_equal([:minus], program)
   end
 
   def test_multiply
     program = PostfixParseString("*")
-    assert_equal(program, [:multiply])
+    assert_equal([:multiply], program)
   end
 
   def test_divide
     program = PostfixParseString("/")
-    assert_equal(program, [:divide])
+    assert_equal([:divide], program)
   end
 
   def test_less
     program = PostfixParseString("<")
-    assert_equal(program, [:less])
+    assert_equal([:less], program)
   end
 
   def test_equal
     program = PostfixParseString("==")
-    assert_equal(program, [:equals])
+    assert_equal([:equals], program)
   end
 
   def test_assign
     program = PostfixParseString("=")
-    assert_equal(program, [:assign])
+    assert_equal([:assign], program)
   end
 
   def test_duplicate
     program = PostfixParseString("duplicate")
-    assert_equal(program, [:duplicate])
+    assert_equal([:duplicate], program)
   end
 
   def test_goto
     program = PostfixParseString("goto")
-    assert_equal(program, [:goto])
+    assert_equal([:goto], program)
   end
 
   def test_if
     program = PostfixParseString("if")
-    assert_equal(program, [:if])
+    assert_equal([:if], program)
   end
 
   def test_print
     program = PostfixParseString("print")
-    assert_equal(program, [:print])
+    assert_equal([:print], program)
   end
 
   def test_swap
     program = PostfixParseString("swap")
-    assert_equal(program, [:swap])
+    assert_equal([:swap], program)
   end
 
   def test_pop
     program = PostfixParseString("pop")
-    assert_equal(program, [:pop])
+    assert_equal([:pop], program)
   end
 
   def test_booleans
     program = PostfixParseString("true")
-    assert_equal(program, [:true])
+    assert_equal([:true], program)
 
     program = PostfixParseString("false")
-    assert_equal(program, [:false])
+    assert_equal([:false], program)
   end
 
   def test_reference
     program = PostfixParseString("reference")
-    assert_equal(program, [:reference])
+    assert_equal([:reference], program)
   end
 
   def test_assign_to_reference
     program = PostfixParseString("assign_to_reference")
-    assert_equal(program, [:assign_to_reference])
+    assert_equal([:assign_to_reference], program)
   end
 
   def test_reference_value
     program = PostfixParseString("reference_value")
-    assert_equal(program, [:reference_value])
+    assert_equal([:reference_value], program)
   end
 
   def test_delete_reference
     program = PostfixParseString("delete_reference")
-    assert_equal(program, [:delete_reference])
+    assert_equal([:delete_reference], program)
   end
 
   def test_and
     program = PostfixParseString("and")
-    assert_equal(program, [:and])
+    assert_equal([:and], program)
   end
 
   def test_or
     program = PostfixParseString("or")
-    assert_equal(program, [:or])
+    assert_equal([:or], program)
   end
 
   def test_not
     program = PostfixParseString("not")
-    assert_equal(program, [:not])
+    assert_equal([:not], program)
   end
 
   def PostfixParseFile
     program = PostfixParseFile("test_postfixsource.pf")
-    assert_equal(program, [7, 4, 3, :less, :if, 1, 3, 2, :swap, :exit])
+    assert_equal([7, 4, 3, :less, :if, 1, 3, 2, :swap, :exit], program)
   end
 
   #~ def comments
     #~ program = PostfixParseString("1(comment 1) 3 swap(comment 2, comment 3) exit (comment4)")
-    #~ assert_equal(program, [1, 3, :swap, :exit])
+    #~ assert_equal([1, 3, :swap, :exit], program)
 
     #~ program = PostfixParseStringDebug("1(comment 1) 3 swap(comment 2, comment 3) exit (comment4)")
-    #~ assert_equal(program, [[1, "comment 0"], [3, nil], [:swap, "comment 2, comment 3"], [:exit, "comment4"]])
+    #~ assert_equal([[1, "comment 0"], [3, nil], [:swap, "comment 2, comment 3"], [:exit, "comment4"]], program)
   #~ end
 end
--- Test_Stack.rb
+++ Test_Stack.rb
@@ -9,98 +9,98 @@ class Test_Stack < Test::Unit::TestCase
 
 
   def test_exit
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("1 exit 2")
     @engine.eat(program)
-    assert_equal(@engine, [1])
+    assert_equal([1], @engine)
   end
 
   def test_plus
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("1 3 + exit")
     @engine.eat(program)
-    assert_equal(@engine, [4])
+    assert_equal([4], @engine)
   end
 
   def test_minus
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("1 3 - exit")
     @engine.eat(program)
-    assert_equal(@engine, [-2])
+    assert_equal([-2], @engine)
   end
 
   def test_multiply
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("13 3 * exit")
     @engine.eat(program)
-    assert_equal(@engine, [39])
+    assert_equal([39], @engine)
   end
 
   def test_divide
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("9 3 / exit")
     @engine.eat(program)
-    assert_equal(@engine, [3])
+    assert_equal([3], @engine)
   end
 
   def test_less
     @engine.reset
     program = PostfixParseString("9 3 < exit")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("3 3 < exit")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("2 3 < exit")
     @engine.eat(program)
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
   end
 
   def test_equals
     @engine.reset
     program = PostfixParseString("9 3 == exit")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("3 3 == exit")
     @engine.eat(program)
     p program
     p @engine.viewlunch
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
 
     @engine.reset
     program = PostfixParseString("2 3 == exit")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
   end
 
   def test_assign
-    @engine.clear
+    @engine.reset
     p @engine
     program = PostfixParseString("1 1 1 2 = exit")
     @engine.eat(program)
-    assert_equal(@engine, [2, 1])
+    assert_equal([2, 1], @engine)
   end
 
   def test_duplicate
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("42 0 duplicate exit")
     @engine.eat(program)
-    assert_equal(@engine, [42, 42])
+    assert_equal([42, 42], @engine)
   end
 
   def test_goto
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("0 1 5 goto 10 2 3 exit")
     @engine.eat(program)
     lunch = @engine.viewlunch.collect {| el | el[1]}
-    assert_equal(@engine, [0, 1, 2, 3])
-    assert_equal(lunch, [0, 1, 5, :goto, 2, 3, :exit])
+    assert_equal([0, 1, 2, 3], @engine)
+    assert_equal([0, 1, 5, :goto, 2, 3, :exit], lunch)
   end
 
   def test_if
@@ -108,154 +108,154 @@ class Test_Stack < Test::Unit::TestCase
     program = PostfixParseString("4 true if 10 11 exit")
     @engine.eat(program)
     lunch = @engine.viewlunch.collect {| el | el[1]}
-    assert_equal(@engine, [10, 11])
-    assert_equal(lunch, [4, :true, :if, 10, 11, :exit])
+    assert_equal([10, 11], @engine)
+    assert_equal([4, :true, :if, 10, 11, :exit], lunch)
 
     @engine.reset
     program = PostfixParseString("4 false if 10 11 exit")
     @engine.eat(program)
     lunch = @engine.viewlunch.collect {| el | el[1]}
-    assert_equal(@engine, [11])
-    assert_equal(lunch, [4, :false, :if, 11, :exit])
+    assert_equal([11], @engine)
+    assert_equal([4, :false, :if, 11, :exit], lunch)
   end
 
   def test_print
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("1 42 print 2 exit")
     @engine.eat(program)
-    assert_equal(@engine, [1, 2])
+    assert_equal([1, 2], @engine)
   end
 
   def test_swap
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("1 3 2 swap 4 exit")
     @engine.eat(program)
-    assert_equal(@engine, [1, 2, 3, 4])
+    assert_equal([1, 2, 3, 4], @engine)
   end
 
   def test_pop
-    @engine.clear
+    @engine.reset
     program = PostfixParseString("1 3 pop 2 4 pop 3 4 exit")
     @engine.eat(program)
-    assert_equal(@engine, [1, 2, 3, 4])
+    assert_equal([1, 2, 3, 4], @engine)
   end
 
   def test_reference
     @engine.reset
     program = PostfixParseString("1 reference")
     @engine.eat(program)
-    assert_equal(@engine, [1])
-    assert_equal(@engine.heap, {1 => nil})
+    assert_equal([1], @engine)
+    assert_equal({1 => nil}, @engine.heap)
 
     @engine.reset
     program = PostfixParseString("1 reference 2 reference")
     @engine.eat(program)
-    assert_equal(@engine, [1, 2])
-    assert_equal(@engine.heap, {1 => nil, 2 => nil, 3 => nil})
+    assert_equal([1, 2], @engine)
+    assert_equal({1 => nil, 2 => nil, 3 => nil}, @engine.heap)
 
     @engine.reset
     program = PostfixParseString("2 reference 1 reference")
     @engine.eat(program)
-    assert_equal(@engine, [1, 3])
-    assert_equal(@engine.heap, {1 => nil, 2 => nil, 3 => nil})
+    assert_equal([1, 3], @engine)
+    assert_equal({1 => nil, 2 => nil, 3 => nil}, @engine.heap)
   end
 
   def test_assign_to_reference
     @engine.reset
     program = PostfixParseString("1 reference 100 assign_to_reference")
     @engine.eat(program)
-    assert_equal(@engine, [])
-    assert_equal(@engine.heap, {1 => 100})
+    assert_equal([], @engine)
+    assert_equal({1 => 100}, @engine.heap)
 
     @engine.reset
     program = PostfixParseString("2 reference 1 + 100 assign_to_reference")
     @engine.eat(program)
-    assert_equal(@engine, [])
-    assert_equal(@engine.heap, {1 => nil, 2 => 100})
+    assert_equal([], @engine)
+    assert_equal({1 => nil, 2 => 100}, @engine.heap)
   end
 
   def test_reference_value
     @engine.reset
     program = PostfixParseString("1 reference 0 duplicate 100 assign_to_reference reference_value")
     @engine.eat(program)
-    assert_equal(@engine, [100])
-    assert_equal(@engine.heap, {1 => 100})
+    assert_equal([100], @engine)
+    assert_equal({1 => 100}, @engine.heap)
 
     @engine.reset
     program = PostfixParseString("2 reference 1 + 0 duplicate 100 assign_to_reference reference_value")
     @engine.eat(program)
-    assert_equal(@engine, [100])
-    assert_equal(@engine.heap, {1 => nil, 2 => 100})
+    assert_equal([100], @engine)
+    assert_equal({1 => nil, 2 => 100}, @engine.heap)
   end
 
   def test_delete_reference
     @engine.reset
     program = PostfixParseString("1 reference 0 duplicate 100 assign_to_reference delete_reference")
     @engine.eat(program)
-    assert_equal(@engine, [])
-    assert_equal(@engine.heap, {})
+    assert_equal([], @engine)
+    assert_equal({}, @engine.heap)
 
     @engine.reset
     program = PostfixParseString("2 reference 0 duplicate 1 + delete_reference delete_reference")
     @engine.eat(program)
-    assert_equal(@engine, [])
-    assert_equal(@engine.heap, {})
+    assert_equal([], @engine)
+    assert_equal({}, @engine.heap)
   end
 
   def test_and
     @engine.reset
     program = PostfixParseString("false false and")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("false true and")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("true false and")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("true true and")
     @engine.eat(program)
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
   end
 
   def test_or
     @engine.reset
     program = PostfixParseString("false false or")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
 
     @engine.reset
     program = PostfixParseString("false true or")
     @engine.eat(program)
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
 
     @engine.reset
     program = PostfixParseString("true false or")
     @engine.eat(program)
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
 
     @engine.reset
     program = PostfixParseString("true true or")
     @engine.eat(program)
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
   end
 
   def test_not
     @engine.reset
     program = PostfixParseString("false not")
     @engine.eat(program)
-    assert_equal(@engine, [true])
+    assert_equal([true], @engine)
 
     @engine.reset
     program = PostfixParseString("true not")
     @engine.eat(program)
-    assert_equal(@engine, [false])
+    assert_equal([false], @engine)
   end
 
 end
