62fd68be90b5e8e3b4a79f1c79f380cd3c56fb5b
André Teintang <andte949@student.liu.se>
2010-04-17 12:50:22 +0200

    Implementerat and, or, not, statement list och variabel deklarationer i infixparsern, men genererar inte constraint-noder

--- InfixParser.rb
+++ InfixParser.rb
@@ -1,87 +1,98 @@
 require 'rdparse.rb'
 
 class InfixParser
+  def initialize
+    @InfixParser = Parser.new("infix parser") do
+      token(/\d+/) {|m| m.to_i}
+      token(/[ \t]+/)
+      token(/[\r\n]+/) {:newline}
+      token(/\<=/) {|m| m}
+      token(/\=>/) {|m| m}
+      token(/\==/) {|m| m}
+      token(/\!=/) {|m| m}
+      token(/</) {|m| m}
+      token(/>/) {|m| m}
+      token(/\w+/) {|m| m}
+      token(/./) {|m| m}
 
-	def initialize
-		@p = []
-		@InfixParser = Parser.new("infix parser") do
-		token(/\d+/) {|m| m.to_i}
-		token(/\s+/) 
-		token(/\<=/) {|m| m}
-		token(/\=>/) {|m| m}
-		token(/\==/) {|m| m}
-		token(/\!=/) {|m| m}
-		token(/</) {|m| m}
-		token(/>/) {|m| m}
-		token(/\w+/) {|m| m}
-		token(/./) {|m| m}
-		
-=begin		
-		start :program do
-			match(:statement_list) {|m| m + " exit"}
-		end
-		
-		rule :statement_list do
-			match(:statement, :statement_list) {|a, b| a + " " + b}
-			match(:statement) {|a| a}
-		end
-		
-		rule :statement do
-			match(:declaration) {|a| a}
-			match(:plus_expr) {|a| a}
-		end
-		
-		rule :declaration do
-			match("integer", /\w+/ ,"=", :plus_expr) {|_,_,_,a| a}
-		end
-		
+      start :program do
+        match(:statement_list) {|m| "{statement_list \n#{m}\n}"}
+      end
 
-		start :comparison do
-			match(:or_expr,"<=", :or_expr){|m, _, n| m<=n}
-			match(:or_expr,"=>", :or_expr){|m, _, n| m=>n}
-			match(:or_expr,"==", :or_expr){|m, _, n| m==n}
-			match(:or_expr,"!=", :or_expr){|m, _, n| m!=n}
-			match(:or_expr,"<", :or_expr){|m, _, n| m<n}
-			match(:or_expr,">", :or_expr){|m, _, n| m>n}
-			match(:or_expr) {|m| m}
-		end
-	
-		rule :or_expr do
-		
-		end
-=end
+      rule :statement_list do
+        match(:statement, :statement_list) {|a, b| "#{a}\n#{b}"}
+        match(:statement) {|m| "#{m}"}
+      end
 
-		start :plus_expr do
-			match(:parenthesis){|m| m}
-			match(:multiply_expr) {|m| m}
-			match(:plus_expr, "+", :multiply_expr) {|a,_,b| a + " " + b +" +"}
-			match(:plus_expr, "-", :multiply_expr) {|a,_,b| a + " " + b +" -"}
-		end
+      rule :statement do
+        match(:variable_declaration, :newline) {|a, b| a}
+      end
 
-		rule :multiply_expr do
-			match(:expression_value) {|m|m}
-			match(:multiply_expr, "*", :expression_value){|a,_,b| a + " " + b+ " *"}
-			match(:multiply_expr, "/", :expression_value){|a,_,b| a + " " + b +" /"}
-		end
+      rule :variable_declaration do
+        match(:datatypes, :identifier, "=", :expression) {|a, b, _, c| "[#{a}:#{b} = #{c}]"}
+      end
 
-		rule  :expression_value do
-			match("(", :plus_expr, ")") {|_,m,_| m}
-			match(Fixnum)  {|m| m.to_s}
-		end
-	end
-end
+      rule :datatypes do
+        match(:identifier) {|a| a}
+      end
+
+      rule :expression do
+        match(:or_expr) {|m| m}
+      end
+
+      rule :or_expr do
+        match(:or_expr, "or", :and_expr) {|l, op, r| wrap(l, op, r)}
+        match(:and_expr) {|m| m}
+      end
+
+      rule :and_expr do
+        match(:and_expr, "and", :not_expr) {|l, op, r| wrap(l, op, r)}
+        match(:not_expr) {|m| m}
+
+      end
+
+      rule :not_expr do
+        match("not", :not_expr) {|op, a| "(#{op} #{a})"}
+        match(:comparison_expr) {|m| m}
+      end
+
+      rule :comparison_expr do
+        match(:comparison_expr,"<=", :plus_expr) {|l, op, r| wrap(l, op, r)}
+        match(:comparison_expr,"=>", :plus_expr) {|l, op, r| wrap(l, op, r)}
+        match(:comparison_expr,"==", :plus_expr) {|l, op, r| wrap(l, op, r)}
+        match(:comparison_expr,"!=", :plus_expr) {|l, op, r| wrap(l, op, r)}
+        match(:comparison_expr,"<", :plus_expr) {|l, op, r| wrap(l, op, r)}
+        match(:comparison_expr,">", :plus_expr) {|l, op, r| wrap(l, op, r)}
+        match(:plus_expr) {|m| m}
+      end
+
+      rule :plus_expr do
+        match(:plus_expr, "+", :multiply_expr) {|l, op, r| wrap(l, op, r)}
+        match(:plus_expr, "-", :multiply_expr) {|l, op, r| wrap(l, op, r)}
+        match(:multiply_expr) {|m| m}
+      end
+
+      rule :multiply_expr do
+        match(:multiply_expr, "*", :expression_value){|l, op, r| wrap(l, op, r)}
+        match(:multiply_expr, "/", :expression_value){|l, op, r| wrap(l, op, r)}
+        match(:expression_value) {|m| m}
+      end
+
+      rule  :expression_value do
+        match("(", :expression, ")") {|_, m, _| "#{m}"}
+        match(Fixnum)  {|m| m.to_s}
+        match(:identifier) {|m| m}
+      end
+
+      rule :identifier do
+        match(/\w+/) {|m| m}
+      end
+    end
+  end
 
   def parse_string(str)
     @InfixParser.parse str
   end
-
-  #~ def log(state = false)
-    #~ if state
-      #~ @diceParser.logger.level = Logger::DEBUG
-    #~ else
-      #~ @diceParser.logger.level = Logger::WARN
-    #~ end
-  #~ end
 end
 
 def InfixParseString(infix)
@@ -93,4 +104,8 @@ def InfixParseFile(sourcefile)
   source = open(sourcefile)
   p = InfixParser.new
   p.parse_string source.read
+end
+
+def wrap(a, op, b)
+  "(#{a} #{op} #{b})"
 end
---
+++ Test_InfixParser.rb
@@ -0,0 +1,6 @@
+require 'test/unit'
+require 'Stack.rb'
+require 'InfixParser.rb'
+
+class Test_InfixParser < Test::Unit::TestCase
+end
